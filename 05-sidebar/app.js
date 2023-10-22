8e2f9a5b389f08e7c98436d328f64d10f7f151f7
rishabangadi <45505864+rishabangadi@users.noreply.github.com>
2023-10-22 22:12:50 +0530

    Added responsive sidebar project

---
+++ 05-sidebar/app.js
@@ -0,0 +1,11 @@
+const sidebarToggle = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+sidebarToggle.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+    sidebar.classList.remove("show-sidebar");
+  });
